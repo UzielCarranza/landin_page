669ee3abb19b3f305eec75d6dd3293a9513a57c6
UzielCarranza <uzielcarranzac97@gmail.com>
2023-02-14 19:58:37 -0500

    adds: initial contact form structure

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,45 @@
+import {useState} from "react";
+import '../styles/contactForm.css';
+
 export const Contact = () => {
+    const [formStatus, setFormStatus] = useState('Send')
+    const onSubmit = (e) => {
+        e.preventDefault()
+        setFormStatus('Submitting...')
+        const {name, email, message} = e.target.elements
+        let conFom = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
+        }
+        console.log(conFom)
+    }
     return (
-        <h1>Contact</h1>
+        <div className="form-container">
+            <h2 className="mb-3">React Contact Form Component Example</h2>
+            <form onSubmit={onSubmit}>
+                <div className="input-wrapper">
+                    <label className="form-label" htmlFor="name">
+                        Name
+                    </label>
+                    <input className="form-control" type="text" id="name" required/>
+                </div>
+                <div className="input-wrapper">
+                    <label className="form-label" htmlFor="email">
+                        Email
+                    </label>
+                    <input className="form-control" type="email" id="email" required/>
+                </div>
+                <div className="input-wrapper ">
+                    <label className="form-label" htmlFor="message">
+                        Message
+                    </label>
+                    <textarea className="form-control" id="message" required/>
+                </div>
+                <button className="form-btn" type="submit">
+                    {formStatus}
+                </button>
+            </form>
+        </div>
     )
 }
