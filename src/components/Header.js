b9b95555907a3bc03f4ddc0d03185a56f56dd82f
UzielCarranza <uzielcarranzac97@gmail.com>
2023-02-27 20:33:17 -0500

    initial set up header structure

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+export const Header = (props) => {
+    return (
+        <header id="header">
+            <div className="intro">
+                <div className="col-md-8 col-md-offset-2 intro-text">
+                    <h1>
+                        title
+                    </h1>
+                    <p>Text</p>
+                    <button>
+                        Learn More
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+}
